a80e773b2225cf8f41a78e959f60303d10e8db4f
friedjof noweck <friedjof@noweck.info>
2022-08-08 08:42:20 +0200

    init project

---
+++ main.py
@@ -0,0 +1,6 @@
+from sandbox.helloWorld import HelloWorld
+
+
+if __name__ == '__main__':
+    helloWorld = HelloWorld()
+    helloWorld.say_hello()
---
+++ sandbox/helloWorld.py
@@ -0,0 +1,6 @@
+class HelloWorld:
+    def __init__(self):
+        self.message = "Hello World!"
+
+    def say_hello(self):
+        print(self.message)
